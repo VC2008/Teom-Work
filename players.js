3b26d7565babf5db29baff04a374c07ddbe2e8ab
VC2008 <526Vcolon@frhsd.com>
2025-05-12 10:42:40 -0400

    first commit

---
+++ players.js
@@ -0,0 +1,38 @@
+const players = [
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'template',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+     {
+        species: 'template',
+        nickname: 'template',
+        elements: 'template',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+     {
+        species: 'template',
+        nickname: 'template',
+        elements: 'template',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+     {
+        species: 'template',
+        nickname: 'template',
+        elements: 'template',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+     {
+        species: 'template',
+        nickname: 'template',
+        elements: 'template',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+
+]
