dce09503bfff2f64e0d4f74b03c4b49ab992760c
VC2008 <526Vcolon@frhsd.com>
2025-05-14 09:25:42 -0400

    Added all the characters plus some Margin (I'll need more later)

--- players.js
+++ players.js
@@ -2,37 +2,229 @@ const players = [
     {
         species: 'template',
         nickname: 'template',
-        elements: 'template',
+        elements: 'Plasma',
         stability: 'template',
         photo: 'https://picsum.photos/300/150'
     },
      {
         species: 'template',
         nickname: 'template',
-        elements: 'template',
+        elements: 'Shadow',
         stability: 'template',
         photo: 'https://picsum.photos/300/150'
     },
      {
         species: 'template',
         nickname: 'template',
-        elements: 'template',
+        elements: 'Mech',
         stability: 'template',
         photo: 'https://picsum.photos/300/150'
     },
      {
         species: 'template',
         nickname: 'template',
-        elements: 'template',
+        elements: 'Crystal',
         stability: 'template',
         photo: 'https://picsum.photos/300/150'
     },
      {
         species: 'template',
         nickname: 'template',
-        elements: 'template',
+        elements: 'Poison',
         stability: 'template',
         photo: 'https://picsum.photos/300/150'
     },
 
+//Double Elementals
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Mech',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Mech',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Mech + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Mech + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+
+//Triple Elementals
+
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Mech',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Mech + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Mech + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Mech + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Mech + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Mech + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+
+//Quad Elementals
+
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Mech + Crystal',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Mech + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Mech + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+    {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Shadow + Mech + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
+
+//Quint
+
+      {
+        species: 'template',
+        nickname: 'template',
+        elements: 'Plasma + Shadow + Mech + Crystal + Poison',
+        stability: 'template',
+        photo: 'https://picsum.photos/300/150'
+    },
 ]
--- roster.js
+++ roster.js
@@ -1,20 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+
     const grid = document.getElementById('rosterGrid')
 
     const render = list => {
         grid.innerHTML = ''
         list. forEach(p => {
             const col = document.createElement('div')
-            col.className = 'col-6 col-lg-2'
+            col.className = 'col-6 col-lg-2 mt-5'
 
             col.innerHTML =`
             <div class="card h-100 shadow-sm">
-                    <img src="${p.photo}" alt="${p.firstName} ${p.lastName}" class="card-img-top"
+                    <img src="${p.photo}" alt="${p.species} ${p.nickname}" class="card-img-top"
                     <div class="card-body text-center"></div>
-                    <h5 class="card-title mb-1">${p.firstName} ${p.lastName}</h5>
-                    <div class="badge badge-position badge-pos-${p.position}">${p.position}</div>
-                    <p class="small text-muted mb-0">Age ${p.stability}</p>
+                    <h5 class="card-title mb-1">${p.species} ${p.nickname}</h5>
+                    <div class="badge badge-position badge-pos-${p.elements}">${p.elements}</div>
+                    <p class="small text-muted mb-0"> ${p.stability}</p>
             ` 
             grid.appendChild(col)
         })
